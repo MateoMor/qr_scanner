b137f70cccc61d753abe8feec6d945f0a874c455
MateoMor <frisded555@gmail.com>
2023-12-14 21:41:44 -0500

    SafeAreaView implement to prevent overflow to buttons

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
-import { StyleSheet, View } from "react-native";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 import Routes from "./src/routes/Routes";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Routes/>
-      {/* <Scanner /> */}
-      <StatusBar style="light" />
-    </View>
+    /* SafeAreaView to prevent the use of buttons space */
+    <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight || 0 }}>
+      <View style={styles.container}>
+        <Routes />
+        {/* <Scanner /> */}
+        <StatusBar style="light" />
+      </View>
+    </SafeAreaView>
   );
 }
 
